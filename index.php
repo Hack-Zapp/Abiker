53ba5b009e74c006309ef7e0dda831889c601a36
lalotone <lalotone@gmail.com>
2015-07-11 20:35:29 +0200

    Adding some stuff

--- index.php
+++ index.php
@@ -5,10 +5,35 @@
 </head>
   <body>
     <div id="slideOne" class="slide">
-
+    <div id="nav-menu">
+        <a href="#slideOne"><span class="redFont">¿Q</span>ue es Zarabikers?</a>  -
+        <a href="#slideTwo"><span class="redFont">M</span>apa</a> -
+        <a href="#slideThree"><span class="redFont">S</span>ocial</a>
+        <!--<a><span class="redFont">E</span>stas</a>-->
+    </div>
+      <div id="frontTitle">
+        <img src="images/zarabikers_logo.png"/>
+      </div>
     </div>
     <div id="slideTwo" class="slide">
-
+      <div id="secondSlideTitle">
+        ¿Que es <span class="redFont">Zara</span>bikers?
+      </div>
+      <div id="presentationImages">
+        <div id="imgPresOne" class="presIcons">
+            <img src="images/map_icon.png" class="presentation"/>
+        </div>
+        <div id="imgPresTwo" class="presIcons">
+            <img src="images/people.png" class="presentation"/>
+        </div>
+        <div id="textOne">
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </div>
+      </div>
     </div>
     <div id="slideThree" class="slide">
 
